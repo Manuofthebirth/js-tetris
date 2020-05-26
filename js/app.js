1b1b263c535078fb802ae289a4440f6cdca63709
Manoel do Nascimento <manu3105@yahoo.com>
2020-05-26 19:26:26 -0300

    added grid query selectors

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const grid = document.querySelector('.grid');
+  let squares = Array.from(document.querySelectorAll('.grid div'));
+  const scoreDisplay: document.querySelector('.score-num');
+  const startBtn: document.querySelector('.start-btn');
+  const width = 10;
+
+  console.log(squares);
 
 })
