2c0f0680c5765284c77820e4524f85c1d9064fb4
Manoel do Nascimento <manu3105@yahoo.com>
2020-05-27 10:57:07 -0300

    tetrominos created

--- js/app.js
+++ js/app.js
@@ -1,10 +1,46 @@
 document.addEventListener('DOMContentLoaded', () => {
   const grid = document.querySelector('.grid');
   let squares = Array.from(document.querySelectorAll('.grid div'));
-  const scoreDisplay: document.querySelector('.score-num');
-  const startBtn: document.querySelector('.start-btn');
+  const scoreDisplay = document.querySelector('.score-num');
+  const startBtn = document.querySelector('.star-btn');
   const width = 10;
 
-  console.log(squares);
+  // Tetrominos
+  const lBlock = [
+    [1, width+1, width*2+1, 2],
+    [width, width+1, width*2, width*2+2],
+    [1, width+1, width*2+1, width*2],
+    [width, width*2, width*2+1, width*2+2]
+  ] 
+
+  const sBlock = [
+    [width*2, width*2+1, width+1, width+2],
+    [0, width, width+1, width*2+1],
+    [width*2, width*2+1, width+1, width+2],
+    [0, width, width+1, width*2+1]
+  ]
+
+  const tBlock = [
+    [width, width+1, 1, width+2],
+    [1, width+1, width+2, width*2+1],
+    [width, width+1, width*2+1, width+2],
+    [1, width+1, width, width*2+1]
+  ]
+  
+  const oBlock = [
+    [0, 1, width, width+1],
+    [0, 1, width, width+1],
+    [0, 1, width, width+1],
+    [0, 1, width, width+1]
+  ]
+  
+  const iBlock = [
+    [1, width+1, width*2+1, width*3+1],
+    [width, width+1, width+2, width+3],
+    [1, width+1, width*2+1, width*3+1],
+    [width, width+1, width+2, width+3]
+  ]
+
+
 
 })
